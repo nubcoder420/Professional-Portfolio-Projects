bfc110c97e70e22e67f4d74dc99af8c5e7ab64d0
nubcoder420 <138780259+nubcoder420@users.noreply.github.com>
2023-11-29 20:00:56 +0800

    Implement Flask-WTForms

--- Day 87 - Web Development - Todo List Webapp/app.py
+++ Day 87 - Web Development - Todo List Webapp/app.py
@@ -1,9 +1,13 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
+app.config['SECRET_KEY'] = 'c7e3bbf68f5c4a8004fc08ff8858117f'
 db = SQLAlchemy(app)
 
 
@@ -11,31 +15,31 @@ class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
 
+class TaskForm(FlaskForm):
+    task = StringField('Task', validators=[DataRequired()])
+    submit = SubmitField('Add Task')
+
 #----- run once to create db -----#
-with app.app_context():
-    db.create_all()
+# with app.app_context():
+#     db.create_all()
 
 @app.route('/')
 def index():
     tasks = Task.query.all()
-    return render_template('index.html', tasks=tasks)
+    form = TaskForm()
+    return render_template('index.html', tasks=tasks, form=form)
 
 @app.route('/add', methods=['POST'])
 def add_task():
-    new_task_content = request.form.get('task')
-    new_task = Task(content=new_task_content)
-    db.session.add(new_task)
-    db.session.commit()
+    form = TaskForm(request.form)
+    if form.validate_on_submit:
+        new_task_content = form.task.data
+        new_task = Task(content=new_task_content)
+        db.session.add(new_task)
+        db.session.commit()
     return redirect(url_for('index'))
-
 
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-
-
-
