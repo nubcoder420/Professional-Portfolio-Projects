15e9fe27635684c231e30c91394642e57c130f21
nubcoder420 <138780259+nubcoder420@users.noreply.github.com>
2023-11-29 18:41:28 +0800

    Create task.db database

--- Day 87 - Web Development - Todo List Webapp/app.py
+++ Day 87 - Web Development - Todo List Webapp/app.py
@@ -3,11 +3,30 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
+db = SQLAlchemy(app)
 
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+
+#----- run once to create db -----#
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    tasks = Task.query.all()
+    return render_template('index.html', tasks=tasks)
+
+@app.route('/add', methods=['POST'])
+def add_task():
+    new_task_content = request.form.get('task')
+    new_task = Task(content=new_task_content)
+    db.session.add(new_task)
+    db.session.commit()
+    return redirect(url_for('index'))
 
 
 
